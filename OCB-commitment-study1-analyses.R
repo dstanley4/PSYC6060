622685449524f524be59eb81c3835671ec895026
David Stanley <dstanley@uoguelph.ca>
2016-09-10 16:30:25 -0400

    add sample analytic header

---
+++ OCB-commitment-study1-analyses.R
@@ -0,0 +1,8 @@
+# Analysis Title: OCB-Commitment Data Analysis
+# Analysis Date (for checkpoint) 2020/01/01:
+# Raw Data File:  OCB-commitment-data-raw.csv
+# Raw Data Codebook File: OCB-commitment-data-raw-codebook.csv
+# Analytic Data File: OCB-commitment-data-analytic.csv
+# Analytic Data Codebook File: OCB-commitment-data-analytic-codebook.csv
+# For paper:  Smith, J., & Smith, S.J. (2021). My sample title
+# Script Author(s): John Smith
