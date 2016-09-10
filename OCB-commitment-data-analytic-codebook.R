4632aadbf4faf2fc0915ca928e987bf45d61921b
David Stanley <dstanley@uoguelph.ca>
2016-09-10 16:08:31 -0400

    Example data codebook

---
+++ OCB-commitment-data-analytic-codebook.R
@@ -0,0 +1,14 @@
+#' @title OCB-Commitment Analytic Datas
+#' @docType data
+#' @name ocb_analytic_data 
+#' @description  Data collected from ABC Company Survey conducted on date YYYY/MM/DD
+#' @format A data frame with 200 rows and 4 variables:
+#' \describe{
+#'   \item    {ocb}{Organizational Citizenship Behavior scores (1 to 5) - Eisenberger, Huntington, Hutchison, Sowa, D. (1986)}
+#'   \item     {ac}{Affective Commitment scores (1 to 5) - Allen, Meyer, & Smith (1993)}
+#'   \item     {cc}{Continuance Commitment scores (1 to 5) - Allen, Meyer, & Smith (1993)}
+#'   \item     {nc}{Normative Commitment scores (1 to 5) - Allen, Meyer, & Smith (1993)}
+#'   \item {gender}{Gender, 0 = women, 1 = men}
+#' }
+#' @references Not applicable
+#' @source \url{Not applicable}
